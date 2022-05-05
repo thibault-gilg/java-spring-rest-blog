bfcc6f929fd8d4093fff62b3aafdf70b468fe27c
Thibault GILG <thibault.gilg@cpe.fr>
2022-05-05 16:54:12 +0200

    module 4

--- src/main/java/com/pluralsight/blog/model/Post.java
+++ src/main/java/com/pluralsight/blog/model/Post.java
@@ -5,18 +5,20 @@ import org.hibernate.annotations.CreationTimestamp;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.lang.reflect.Method;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
+    @NotNull
+    @Size(min = 4, max = 100)
     private String title;
     @Column(length=1000000)
     @Lob
